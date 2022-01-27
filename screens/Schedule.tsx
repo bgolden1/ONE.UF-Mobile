0db7ff1de0a0232b89df0ace6519971e41aa61c4
bgolden1 <brodygolden7@gmail.com>
2022-01-27 15:16:08 -0500

    basic api call working

--- screens/Schedule.tsx
+++ screens/Schedule.tsx
@@ -1,10 +1,25 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import { Text, View } from '../components/Themed';
+import {Text, View } from '../components/Themed';
 
 export default function Schedule() {
+  const [isLoading, setLoading] = useState(true);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://one.uf.edu/apix/soc/filters").then((res) => {
+      console.log(res);
+      setCategories(res.data['categories']);
+      setLoading(false);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }, []);
   return (
     <View style={styles.container}>
+      {isLoading?<Text style={styles.title}>Loading</Text> : <Text>Category 1: {categories[0]["DESC"]}</Text>}
     </View>
   );
 }
