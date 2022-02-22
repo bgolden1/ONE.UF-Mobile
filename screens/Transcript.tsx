47c1b8bde0de5b632a48e8c80ffed1822e71d529
bgolden1 <brodygolden7@gmail.com>
2022-02-22 16:08:31 -0500

    might've done a thing

---
+++ screens/Transcript.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import { Text, View } from '../components/Themed';
+
+export default function Transcript() {
+    return (
+        <Text>This is gonna be sick</Text>
+    );
+}
