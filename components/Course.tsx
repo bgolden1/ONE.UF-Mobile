cb53807f37d82d19ac777cfa96b20b25356d42c2
bgolden1 <brodygolden7@gmail.com>
2022-02-24 12:05:19 -0500

    changes to soc

--- components/Course.tsx
+++ components/Course.tsx
@@ -1,27 +1,47 @@
 import { Text, useThemeColor, View } from '../components/Themed';
 import { StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 export function Courses(props: any) {
     const courses = props['courses'];
-
-
-    function onPress(course: any) {
-        
-    }
 
     return (
         <ScrollView style={styles.scrollView} contentContainerStyle={styles.contentContainer}>
             {courses.map((course: any, key: any) => {
                 return (
-                    <TouchableOpacity onPress={() => onPress(course)} key={key}>
-                        <View key={key} style={styles.course}>
-                            <Text style={styles.title}>{course.code}: {course.name}</Text>
-                            <Text style={styles.body}>{course.description}</Text>
-                        </View>
-                    </TouchableOpacity>);
+                    <Course course={course} key={key} />
+                );
             })}
         </ScrollView>
     );
+}
+
+function Course(props: any) {
+    const [isPressed, press] = useState(false);
+    const course = props['course'];
+    const key = props['key'];
+
+    function onPress(key: any) {
+        press(!isPressed);
+    }
+    return (
+        <TouchableOpacity onPress={() => onPress(key)} key={key}>
+            <View key={key} style={styles.course}>
+                <Text style={styles.title}>{course.code}: {course.name}</Text>
+                <Text style={styles.body}>{course.description}</Text>
+                {course.prerequisites != '' && 
+                <Text style={styles.body}>{"\n"}{course.prerequisites}</Text>}
+                {isPressed &&
+                    course.sections.map((section: any, newKey: any) => {
+                        return (
+                        <View style={styles.section} key={newKey}>
+                            <Text style={styles.title}> Class # {section.classNumber}</Text>
+                        </View>
+                        );
+                    })
+                }
+            </View>
+        </TouchableOpacity>);
 }
 
 
@@ -42,6 +62,15 @@ const styles = StyleSheet.create({
         alignSelf: 'flex-start',
         width: '95%',
         borderColor: '#285697',
+        borderWidth: 2,
+        borderRadius: 15,
+        marginBottom: 10,
+        padding: 5
+    },
+    section: {
+        alignSelf: 'flex-start',
+        width: '95%',
+        borderColor: 'grey',
         borderWidth: 2,
         borderRadius: 15,
         marginBottom: 10,
