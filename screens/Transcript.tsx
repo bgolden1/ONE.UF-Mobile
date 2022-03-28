5ce3ddff1f171b56aa32884b7e332c8b04198978
bgolden1 <brodygolden7@gmail.com>
2022-03-27 23:16:44 -0400

    transcript changes

--- screens/Transcript.tsx
+++ screens/Transcript.tsx
@@ -5,13 +5,55 @@ import { StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import { Text, View } from '../components/Themed';
 
 export default function Transcript() {
-    const [transcript, setTranscript] = useState([])
+    const [transcript, setTranscript] = useState({
+        personalInfo: {
+            name: "",
+            ufid: 0,
+            residency: "",
+            residencyDescription: "",
+            basisOfAdmissionCode: "",
+            basisOfAdmissionDescription: "",
+            ssn: "",
+            dob: ""
+        },
+        records: {
+            undergraduate: {
+                comments: {
+                    before: {
+                        2: {
+                            1: ""
+                        },
+                        3: {
+                            1: ""
+                        }
+                    }
+                },
+                ufGpa: "",
+                totalHoursEarned: "",
+                gradePointsEarned: "",
+                ufHoursEarned: "",
+                ufHoursCarried: "",
+                transferHoursEarned: "",
+                terms: [{
+                    
+                }],
+                concentrations: "",
+                programs: [],
+                careerDescription: ""
+            }
+        }
+    })
     useEffect(() => {
-        const url = "google.com"
-        axios.get(url).then((res) => {
-
-        }).catch((err)=> {
-            
+        const url = "http://34.136.6.158:5000/api/unofficialtranscript"
+        const headers = { 
+            'X-UF-Cookie': '_shibsession_68747470733a2f2f73712e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f68747470733a2f2f73702e6c6f67696e2e75666c2e6564752f75726e3a6564753a75666c3a70726f643a30303734312f=_CHARLES_',
+            'X-Host-Choice': 'mock-host'
+        }
+        axios.get(url, { headers: headers }).then((res) => {
+            console.log(res.status);
+            setTranscript(res.data);
+        }).catch((err) => {
+            console.log(err);
         });
     });
     return (
@@ -19,43 +61,64 @@ export default function Transcript() {
             <View style={{ alignItems: "center", flex: 1 }}>
                 <View style={styles.personal_info}>
                     <Text style={styles.title}>Personal Info</Text>
-                    <Text style={styles.body}>(Name)</Text>
-                    <Text style={styles.body}>(Basis of admission)</Text>
-                    <Text style={styles.body}>(Residency)</Text>
+                    <Text style={styles.body}>{transcript.personalInfo.name}</Text>
+                    <Text style={styles.body}>{transcript.personalInfo.basisOfAdmissionDescription}</Text>
+                    <Text style={styles.body}>{transcript.personalInfo.residencyDescription}</Text>
                 </View>
                 <View style={styles.separator} />
                 <View style={{ alignItems: "center" }}>
-                    <Text style={styles.title}>(careerDescription) Record</Text>
-                    <Text style={styles.body}>Major (n): (Major)</Text>
+                    <Text style={styles.title}>{transcript.records.undergraduate.careerDescription} Record</Text>
+                    {major_list(transcript.records.undergraduate.programs)}
                 </View>
                 <View style={styles.gpa}>
                     <View style={styles.columns}>
                         <Text>Cumulative GPA:</Text>
-                        <Text>(gpa){"\n"}</Text>
+                        <Text>{transcript.records.undergraduate.ufGpa}{"\n"}</Text>
 
                         <Text>Total Hours:</Text>
-                        <Text>(hours){"\n"}</Text>
+                        <Text>{transcript.records.undergraduate.totalHoursEarned}{"\n"}</Text>
 
                         <Text>UF Hours Carried:</Text>
-                        <Text>(hours_c)</Text>
+                        <Text>{transcript.records.undergraduate.ufHoursCarried}</Text>
                     </View>
                     <View style={styles.columns}>
                         <Text>Cumulative Grade Points:</Text>
-                        <Text>(gradePoints){"\n"}</Text>
+                        <Text>{transcript.records.undergraduate.gradePointsEarned}{"\n"}</Text>
 
                         <Text>UF Cumulative Hours:</Text>
-                        <Text>(cumulative_hours){"\n"}</Text>
+                        <Text>{transcript.records.undergraduate.ufHoursEarned}{"\n"}</Text>
 
                         <Text>Transfer Hours:</Text>
-                        <Text>(t_hours)</Text>
+                        <Text>{transcript.records.undergraduate.transferHoursEarned}</Text>
                     </View>
                 </View>
                 <View style={styles.classes}>
-
+                    {}
                 </View>
             </View>
         </ScrollView>
     );
+}
+
+function major_list (programs: any) {
+    return (
+        programs.map((program: any, key: any) => {
+            return(
+                <View>
+                    <Text style={styles.body}>{program.college}</Text>
+                    {program.plans.map((plan: any, key1: any) => {
+                        return(
+                            <Text style={styles.body}>{plan.planLongName}: {plan.descr}</Text>
+                        )
+                    })}
+                </View>
+            )
+        })
+    )
+}
+
+function class_list (terms: any) {
+    
 }
 
 const styles = StyleSheet.create({
@@ -87,6 +150,7 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         fontWeight: 'bold',
+        textAlign: 'center'
     },
     separator: {
         marginVertical: 30,
