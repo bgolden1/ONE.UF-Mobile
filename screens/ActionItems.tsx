5e02f71c31c43107101d38c8a101d42dc7ab5d64
bgolden1 <brodygolden7@gmail.com>
2022-04-07 00:23:22 -0400

    action items changes

--- screens/ActionItems.tsx
+++ screens/ActionItems.tsx
@@ -3,96 +3,22 @@ import axios from 'axios';
 import { StyleSheet, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native';
 
 import { Text, View } from '../components/Themed';
+import RenderHtml from 'react-native-render-html';
+import { useWindowDimensions } from 'react-native';
 
 export default function ActionItems() {
-    const [pressed, press] = useState(false);
-
-    function onPress() {
-        press(!pressed)
-    }
 
     const [holds, setHolds] = useState({
-    groups:[
-
-        {
-            label: "Holds Diploma &/or Graduation",
-            holds: [
-              {
-                label: "Graduation/Diploma Hold (UFIC)",
-                details: "<p>Please request your host school to send your transcript to UF International Center, Coordinator, Study Abroad Services, 170 HUB, P O Box 113225, Gainesville, FL 36211-3225. Once grades have been posted for your study abroad semester, this hold will be removed.</p><p>Contact: UF International Center</p><p>170 HUB</p><p><a href=\"tel:352.273.1539\">352.273.1539</a></p>",
-                term: "Fall 2021",
-                startTermCode: 2218,
-                endTermCode: "",
-                contactPhone: "",
-                contactEmail: "",
-                startDate: "1902-01-01",
-                holdCode: "/UFLOR/4IN/STUDY",
-                sourceName: "OFFICE ENROLLMENT MANAGEMENT",
-                timeStamp: "2021-09-16-15.36.46.833994"
-              }
-            ]
-          }
-
-
-
-    ],
-    info: [
-
-        {
-            label: "Don't Get Phished",
-            details: "<p><font color=\"#000000\"><font face=\"Calibri\"><font size=\"3\">Please click the below link for a quick ~10 minute review regarding protecting your UF account from phishing attempts.</font></font></font></p><p><font color=\"#000000\"><font face=\"Calibri\"><font size=\"3\">Upon completing the course, this Informational Notice will disappear from your OneUF account.&#xA0; The phishing review</font></font></font></p><p><font color=\"#000000\"><font face=\"Calibri\"><font size=\"3\">will appear on your account once every 365 days.</font></font></font></p><div class=\"WordSection1\"><p><a href=\"https://ufl.instructure.com/courses/417403\" target=\"_blank\"><span><span><span><font color=\"#0066cc\">https://ufl.instructure.com/courses/417403</font></span></span></span></a></p></div>",
-            term: "",
-            startTermCode: "",
-            endTermCode: "",
-            contactPhone: "",
-            contactEmail: "",
-            startDate: "",
-            holdCode: "/UFLOR/R12/CYBER",
-            sourceName: "INFORMATION TECHNOLOGY",
-            timeStamp: "2021-01-26-03.54.23.441617"
-          },
-          {
-            label: "Information from Registrar",
-            details: "<p><span>You are eligible for a transcript fee waiver due to your military-affiliated status.&#xA0;</span></p><p>When you need a transcript, c<span>ontact UF Student Veteran Services at&#xA0;</span><a href=\"mailto:vacounselor@ufl.edu\" target=\"_blank\"><span>vacounselor@ufl.edu</span></a><span>&#xA0;to initiate your transcript request and have the fee waived.</span></p><p><em><span>Do <strong>NOT </strong>submit any transcript orders through ONE.UF.</span></em><br><em><span>Orders placed via ONE.UF or payments submitted are&#xA0;<strong>NOT</strong>&#xA0;refundable.&#xA0;</span></em></p>",
-            term: "",
-            startTermCode: "",
-            endTermCode: "",
-            contactPhone: "",
-            contactEmail: "",
-            startDate: "",
-            holdCode: "/UFLOR/R02/TRNFW",
-            sourceName: "OFFICE ENROLLMENT MANAGEMENT",
-            timeStamp: "2021-01-27-09.15.24.864265"
-          },
-          {
-            label: "Health Insurance Proof",
-            details: "<p><!--StartFragment--></p><p>If you have not done so already, please provide proof of your health insurance for the <strong>Summer/Fall</strong>&#xA0;<b>terms</b>&#xA0;or you will be charged on your student account. <a href=\"https://healthcompliance.shcc.ufl.edu/insurance/comparable-coverage/\" target=\"_blank\">For more information, please visit the Health Compliance Office.</a></p><p><!--StartFragment--><em>To acknowledge that you understand the requirement and clear the action item from your list, </em>click button below.</p><p><!--EndFragment--></p><p>&#xA0;</p><p>&#xA0;</p>",
-            term: "",
-            startTermCode: "",
-            endTermCode: "",
-            contactPhone: "",
-            contactEmail: "",
-            startDate: "",
-            holdCode: "/UFLOR/H02/PROSF",
-            sourceName: "OFFICE ENROLLMENT MANAGEMENT",
-            timeStamp: "2021-06-07-19.25.52.725270"
-          }
-          
-          
-
-
-
-    ]
+        groups: [],
+        info: []
     })
 
     const [todos, setTodos] = useState({
-    groups:[],
-    info: []
+        groups: [],
+        info: []
     })
 
     const [isLoading, setLoading] = useState(true);
-
-
 
     useEffect(() => {
         const url = "http://34.136.6.158:5000/api/"
@@ -105,94 +31,46 @@ export default function ActionItems() {
         }).catch((err) => {
             console.log(err);
         });
-        setLoading(false);
+        axios.get(url + "todo", { headers: headers }).then((res) => {
+            setTodos(res.data);
+            setLoading(false);
+        }).catch((err) => {
+            console.log(err);
+        });
     });
-
-
-
-
 
     return (
         isLoading ? <View style={{ alignSelf: 'center', alignContent: 'center', alignItems: 'center' }}><ActivityIndicator size={'large'} color={'blue'} /></View> :
             <ScrollView>
-
-
-
-
-
-
-                <Text style={styles.title}> Holds </Text>
-                
-
-
                 <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={styles.title}>{"\n"}Meet time(s):</Text>
-                    {holds.groups.map((object: any, key: any) => {
-                    return (
+                    <Text style={styles.title}>{"\n"}Holds:</Text>
+                    {holds.groups.map((hold_type: any, key: any) => {
+                        return (
 
-                        <View style={styles.personal_info}>
-                            
+                            <View style={styles.section}>
 
-                            <TouchableOpacity style={styles.classes} onPress={onPress} key = {'key'}>
+                                <Text style={styles.body}>{hold_type.label}</Text>
 
-                            <Text style={styles.body}>{object.label}</Text>
+                                {hold_type.holds.map((hold: any, key2: any) => {
+                                    return (
+                                        <Holds hold={hold} />
+                                    );
+                                })}
 
-                            {object.holds.map((object2: any, key2: any) => {
+                            </View>
+
+                        );
+                    })}
+
+                    <Text style={styles.title}>{"\n"}Info:</Text>
+
+                    <View style={styles.section}>
+                        {holds.info.map((info: any, key: any) => {
                             return (
-                                
-                                pressed ? 
-
-                                <Text style={styles.body}>{"\n"}Label:{"\n"}{object2.label }{"\n"}{"\n"}Details:{"\n"}{object2.details}{"\n"}{"\n"}
-                                Term:{"\n"}{object2.term}{"\n"}{"\n"}Start Term Code:{"\n"}{object2.startTermCode}{"\n"}{"\n"}End Term Code:{"\n"}{object2.endTermCode}{"\n"}{"\n"}
-                                Contact Phone:{"\n"}{object2.contactPhone}{"\n"}{"\n"}Contact Email:{"\n"}{object2.contactEmail}{"\n"}{"\n"}
-                                Start Date:{"\n"}{object2.startDate}{"\n"}{"\n"}Hold Code:{"\n"}{object2.holdCode}{"\n"}{"\n"}Source Name:{"\n"}{object2.sourceName}{"\n"}{"\n"}
-                                Time Stamp:{"\n"}{object2.timeStamp}</Text>
-
-                                :
-                                <Text style={styles.body}>{"\n"}Label:{"\n"}{object2.label }</Text>
-
-                                );
-                            })}
-
-                            </TouchableOpacity>
-
-                        </View>
-
-                        );
-                    })}
-
-                    <Text style={styles.title}>{"\n"}Personal Info:</Text>
-
-                    <TouchableOpacity style={styles.classes2} onPress={onPress} key = {'key2'}>
-
-                    {holds.info.map((object2: any, key: any) => {
-                    return (
-
-                        pressed ?
-                        <View style={styles.personal_info}>
-                            
-
-                            <Text style={styles.body}>{"\n"}Label:{"\n"}{object2.label }{"\n"}{"\n"}Details:{"\n"}{object2.details}{"\n"}{"\n"}
-                                Term:{"\n"}{object2.term}{"\n"}{"\n"}Start Term Code:{"\n"}{object2.startTermCode}{"\n"}{"\n"}End Term Code:{"\n"}{object2.endTermCode}{"\n"}{"\n"}
-                                Contact Phone:{"\n"}{object2.contactPhone}{"\n"}{"\n"}Contact Email:{"\n"}{object2.contactEmail}{"\n"}{"\n"}
-                                Start Date:{"\n"}{object2.startDate}{"\n"}{"\n"}Hold Code:{"\n"}{object2.holdCode}{"\n"}{"\n"}Source Name:{"\n"}{object2.sourceName}{"\n"}{"\n"}
-                                Time Stamp:{"\n"}{object2.timeStamp}</Text>
-
-                        </View>
-
-                        :
-                        <View style={styles.personal_info}>
-                            
-
-                            <Text style={styles.body}>{"\n"}Label:{"\n"}{object2.label }</Text>
-                        </View>
-
-
-                        );
-                    })}
-
-                    </TouchableOpacity>
-
+                                <Info info={info} />
+                            );
+                        })}
+                    </View>
 
                 </View>
 
@@ -201,8 +79,49 @@ export default function ActionItems() {
 
             </ScrollView>
     );
+
+
 }
 
+function Holds(props: any) {
+    const [pressed, press] = useState(false);
+    const hold = props.hold;
+    const { width } = useWindowDimensions()
+    const details = {
+        html: hold.details
+    }
+    return (
+        <TouchableOpacity onPress={() => press(!pressed)} style={styles.subsection}>
+            <Text style={[styles.body]}>{hold.label}</Text>
+            {pressed &&
+                <View>
+                    <RenderHtml contentWidth={width} source={details} />
+                </View>
+            }
+
+        </TouchableOpacity>
+    )
+}
+
+function Info(props: any) {
+    const [pressed, press] = useState(false);
+    const info = props.info;
+    const { width } = useWindowDimensions()
+    const details = {
+        html: info.details
+    }
+    return (
+        <TouchableOpacity onPress={() => press(!pressed)} style={styles.subsection}>
+            <Text style={[styles.body]}>{info.label}</Text>
+            {pressed &&
+                <View>
+                    <RenderHtml contentWidth={width} source={details} />
+                </View>
+            }
+
+        </TouchableOpacity>
+    )
+}
 
 
 
@@ -212,32 +131,19 @@ const styles = StyleSheet.create({
     personal_info: {
         justifyContent: "flex-start",
         alignItems: "center",
-        borderWidth: 2,
+        borderWidth: 3,
         borderColor: "#285697",
-        marginTop: 15,
+        marginTop: 1,
         borderRadius: 15,
-        padding: 15,
-        paddingTop: 5,
+        paddingBottom: 1,
+        paddingTop: 1,
+        width: '90%'
     },
-    gpa: {
-        justifyContent: "flex-start",
-        alignItems: "center",
-        flexDirection: "row",
-        borderWidth: 2,
-        borderColor: "#285697",
-        marginTop: 15,
-        borderRadius: 15,
-        padding: 15,
-        paddingTop: 5,
-    },
-    columns: {
-        alignItems: 'center',
-        margin: 10,
-    },
+
     title: {
         fontSize: 20,
         fontWeight: 'bold',
-        textAlign: 'center'
+        alignSelf: 'center'
     },
     separator: {
         marginVertical: 30,
@@ -250,12 +156,28 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 17,
     },
-    classes: {
+
+    subsection: {
+        alignSelf: 'center',
+        width: '90%',
+        borderColor: 'grey',
+        borderWidth: 2,
+        borderRadius: 15,
+        marginTop: 10,
+        marginBottom: 10,
+        padding: 5,
+        paddingBottom: 15,
     },
-    classes2: {
-    },
-    class_text: {
-        textAlign: 'left',
-        fontSize: 17,
+    section: {
+        alignSelf: 'center',
+        width: '90%',
+        borderColor: '#285697',
+        borderWidth: 2,
+        borderRadius: 15,
+        marginTop: 10,
+        marginBottom: 10,
+        padding: 5,
+        paddingBottom: 15,
+        alignItems: 'center'
     }
 });
